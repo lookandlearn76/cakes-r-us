0a01dc328bff2c847371691c4b5d22044f29cf25
Mark <lookandlearn76@yahoo.co.uk>
2018-04-02 23:38:10 +0100

    last commit to gh

--- src/components/cake_show.js
+++ src/components/cake_show.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchCake, deleteCake } from "../actions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchCake, deleteCake } from '../actions';
 
 class CakeShow extends Component {
   componentDidMount() {
@@ -13,7 +13,7 @@ class CakeShow extends Component {
     const { id } = this.props.match.params;
 
     this.props.deleteCake(id, () => {
-      this.props.history.push("/");
+      this.props.history.push('/');
     });
   }
 
@@ -26,7 +26,9 @@ class CakeShow extends Component {
 
     return (
       <div className="container">
-        <Link to="/"><div className="btn btn-primary">Back To Index</div></Link>
+        <Link to="/">
+          <div className="btn btn-primary">Back To Index</div>
+        </Link>
         <button
           className="btn btn-danger pull-xs-right"
           onClick={this.onDeleteClick.bind(this)}
@@ -35,7 +37,13 @@ class CakeShow extends Component {
         </button>
         <h1>Cake Name: {cake.name}</h1>
         <h2>Comments: {cake.comment}</h2>
-        <iframe className="embed-responsive-item" src={cake.imageUrl}></iframe>
+        <iframe
+          title={cake.name}
+          className="embed-responsive-item"
+          src={cake.imageUrl}
+        >
+          <img minWidth="100%" src={cake.imageUrl} />
+        </iframe>
         <h3>Yum Factor: {cake.yumFactor}</h3>
       </div>
     );
